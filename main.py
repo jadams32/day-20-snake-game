59c1228decedd41567537235a4b1527373f32af1
John Adams Jr <john.c.adams32@gmail.com>
2022-01-07 13:37:22 -0700

    add snake forward movement

--- main.py
+++ main.py
@@ -4,11 +4,13 @@
 from turtle import Turtle, Screen
 import random
 from playsound import playsound
+import time
 
 screen = Screen()
 screen.setup(width=600, height=600)
 screen.bgcolor("black")
 screen.title("🐍 Classic Snek 🐍")
+screen.tracer(0)
 
 # Create a row of turtles that act as our snake.
 x_coordinates = [0, -20, -40]
@@ -16,16 +18,27 @@ turtles = []
 for turtle in range(3):
     new_snake_part = Turtle(shape="square")
     new_snake_part.color("white")
+    new_snake_part.penup()
     new_snake_part.goto(x=x_coordinates[turtle], y=0)
     turtles.append(new_snake_part)
 
-# TODO: Allow the snake to move.
+screen.update()
+
 # TODO: create snake food.
 # TODO: Detect collision with food.
 # TODO: Create a scoreboard.
 # TODO: Detect collision with wall.
 # TODO: Detect collision with body or tail.
 # TODO: Add music.
+playing = True
+while playing:
+    screen.update()
+    time.sleep(0.1)
+    for segment in range(len(turtles)-1, 0, -1):
+        x_new = turtles[segment - 1].xcor()
+        y_new = turtles[segment - 1].ycor()
+        turtles[segment].goto(x_new, y_new)
+    turtles[0].forward(20)
 
 
 screen.exitonclick()
