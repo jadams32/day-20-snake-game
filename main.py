187c400663ef1f767bca2a12aff02cd4768add06
John Adams Jr <john.c.adams32@gmail.com>
2022-01-07 12:41:49 -0700

    created initial snek and added todo list.

--- main.py
+++ main.py
@@ -3,12 +3,29 @@
 
 from turtle import Turtle, Screen
 import random
+from playsound import playsound
 
 screen = Screen()
+screen.setup(width=600, height=600)
 screen.bgcolor("black")
+screen.title("🐍 Classic Snek 🐍")
 
-john = Turtle(shape="square")
-john.color("white")
+# Create a row of turtles that act as our snake.
+x_coordinates = [0, -20, -40]
+turtles = []
+for turtle in range(3):
+    new_snake_part = Turtle(shape="square")
+    new_snake_part.color("white")
+    new_snake_part.goto(x=x_coordinates[turtle], y=0)
+    turtles.append(new_snake_part)
+
+# TODO: Allow the snake to move.
+# TODO: create snake food.
+# TODO: Detect collision with food.
+# TODO: Create a scoreboard.
+# TODO: Detect collision with wall.
+# TODO: Detect collision with body or tail.
+# TODO: Add music.
 
 
 screen.exitonclick()
