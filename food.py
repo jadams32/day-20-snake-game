cece907e8a426095857760434f3c83e95806e1df
John Adams Jr <john.c.adams32@gmail.com>
2022-01-07 19:29:04 -0700

    created the food class

--- food.py
+++ food.py
@@ -1,6 +1,16 @@
 from turtle import Turtle
+import random
 
-class Food:
+
+class Food(Turtle):
 
     def __init__(self):
-        self.bite = bite
+        super().__init__()
+        self.shape("circle")
+        self.penup()
+        self.color("red")
+        self.shapesize(stretch_len=.5, stretch_wid=.5)
+        self.speed("fastest")
+        rand_x = random.randint(-280, 280)
+        rand_y = random.randint(-280, 280)
+        self.goto(rand_x, rand_y)
